d07ede2135f5c9c0124b2a12ed5ab38e91d2ced0
VijayRameshkumar <baksvijay@gmail.com>
2020-10-29 22:15:55 +0530

    update

--- computer-vision/computer_vision_Assignments/coin_detection/coin_toss.py
+++ computer-vision/computer_vision_Assignments/coin_detection/coin_toss.py
@@ -36,13 +36,6 @@ img[markers == -1] = [0, 0, 255]
 
 dup = img.copy()
 
-def crop_center(img, new_size):
-    y, x, c = img.shape
-    (cropx, cropy) = new_size
-    startx = x // 2 - (cropx // 2)
-    starty = y // 2 - (cropy // 2)
-    return img[starty:starty + cropy, startx:startx + cropx]
-
 height, width = markers.shape
 
 new_image = []
